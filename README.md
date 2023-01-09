887701d2d49905c73199f0e81362dc8475f380d8
Victor <victormovileanu@gmail.com>
2023-01-09 22:27:14 +0100

    configure colors

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 * markdown: https://stackoverflow.com/questions/1319657/javascript-to-convert-markdown-textile-to-html-and-ideally-back-to-markdown-t
 * tailwind does not recommend "create-react-app": https://tailwindcss.com/docs/guides/create-react-app
-
+* give color names with https://chir.ag/projects/name-that-color
 
 Set up react with parcel
 * https://www.freecodecamp.org/news/how-to-up-a-react-app-with-parcel/
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        woodsmoke: "#151619",
+        shark: "#1D1F22",
+        sharkLight: "#2B2D31",
+        abbey: "#35393F",
+        shuttleGray: "#5A6069",
+        rollingStone: "#7C8187",
+        frenchGray: "#C1C4CB",
+        mercury: "#E4E4E4",
+        wildSand: "#F5F5F5",
+        burntSienna: "#E46643",
+        sandyBrown: "#F39765",
+      }
+    },
   },
   plugins: [],
 }
